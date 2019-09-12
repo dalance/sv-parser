6428d5ae1aaf5895b9fe3037de13d8f631673c2c
dalance <dalance@gmail.com>
2019-09-12 19:15:55 +0900

    Add sv-parser-error

---
+++ sv-parser-error/src/lib.rs
@@ -0,0 +1,71 @@
+use failure::{Backtrace, Context, Fail};
+use std::fmt;
+use std::fmt::Display;
+use std::io::Error as IOError;
+
+// -----------------------------------------------------------------------------
+
+#[derive(Fail, Debug)]
+pub enum ErrorKind {
+    #[fail(display = "IO error")]
+    Io,
+    #[fail(display = "Parse error")]
+    Parse,
+}
+
+// -----------------------------------------------------------------------------
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&dyn Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn new(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+impl From<IOError> for Error {
+    fn from(error: IOError) -> Error {
+        Error {
+            inner: error.context(ErrorKind::Io),
+        }
+    }
+}

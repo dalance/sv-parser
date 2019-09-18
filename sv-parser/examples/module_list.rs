85a15ebc475e8f2c0cf7ce797eec3eaaa84430dc
dalance <dalance@gmail.com>
2019-09-18 21:26:25 +0900

    Add example

--- sv-parser/examples/module_list.rs
+++ sv-parser/examples/module_list.rs
@@ -1,20 +1,3 @@
-# sv-parser
-Parser library for SystemVerilog ([IEEE 1800-2017](https://standards.ieee.org/standard/1800-2017.html)).
-
-[![Actions Status](https://github.com/dalance/sv-parser/workflows/Rust/badge.svg)](https://github.com/dalance/sv-parser/actions)
-[![Crates.io](https://img.shields.io/crates/v/sv-parser.svg)](https://crates.io/crates/sv-parser)
-[![Docs.rs](https://docs.rs/sv-parser/badge.svg)](https://docs.rs/sv-parser)
-
-## Usage
-
-```Cargo.toml
-[dependencies]
-sv-parser = "0.1.0"
-```
-
-## Example
-
-```rust
 use std::collections::HashMap;
 use std::convert::TryInto;
 use std::env;
@@ -83,4 +66,3 @@ fn get_identifier(node: RefNode) -> Option<Locate> {
     }
     None
 }
-```

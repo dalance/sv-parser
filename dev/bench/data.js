1ee3c85c48eb8ad5d89fa9be52b17c2e3e7fbc1c
github-action-benchmark <github@users.noreply.github.com>
2019-11-11 06:44:00 +0000

    add Rust Benchmark (cargo) benchmark result for 62cdd6c277e65b1084d215a43cf47ee447f3d33c

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1573454640660,
+  "repoUrl": "https://github.com/dalance/sv-parser",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dalance@gmail.com",
+            "name": "dalance",
+            "username": "dalance"
+          },
+          "committer": {
+            "email": "dalance@gmail.com",
+            "name": "dalance",
+            "username": "dalance"
+          },
+          "distinct": true,
+          "id": "62cdd6c277e65b1084d215a43cf47ee447f3d33c",
+          "message": "Fix benchmark",
+          "timestamp": "2019-11-11T15:19:19+09:00",
+          "tree_id": "895bb9be4e77eafd7a9bc45ec336182f5221e21a",
+          "url": "https://github.com/dalance/sv-parser/commit/62cdd6c277e65b1084d215a43cf47ee447f3d33c"
+        },
+        "date": 1573454640636,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "test1",
+            "value": 31424213,
+            "range": "+/- 3,982,605",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "test2",
+            "value": 31250741,
+            "range": "+/- 4,593,208",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
